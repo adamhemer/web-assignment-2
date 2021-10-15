27cb5288948f2a356f55cf68acd2648eb0b06799
Adam Hemer <adamhemer32@gmail.com>
2021-10-15 15:25:15 +1030

    Moved js to scripts folder.

--- cart.php
+++ cart.php
@@ -15,7 +15,7 @@
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
     />
-    <script src="cart.js"></script>
+    <script src="scripts/cart.js"></script>
   </head>
 
   <body>
--- shop.php
+++ shop.php
@@ -15,7 +15,7 @@
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
     />
-    <script src="shop.js"></script>
+    <script src="scripts/shop.js"></script>
   </head>
 
   <body>
